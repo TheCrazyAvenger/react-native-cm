e5a2bb018f19292fcf8ef87b6d0f0c24674ea92e
Illia Pauliushchyk <illia.pauliushchyk@itechart-group.com>
2021-12-01 12:16:55 +0300

    Added styling for html

---
+++ src/screens/DetailsNews/tagStyles.ts
@@ -0,0 +1,22 @@
+import {colors} from '../../constants';
+
+export const tagStyles = {
+  p: {
+    fontFamily: 'OpenSans-Regular',
+    fontSize: 16,
+    color: colors.black,
+    lineHeight: 21.79,
+  },
+  h1: {
+    fontFamily: 'OpenSans-Regular',
+    fontSize: 32,
+    color: colors.black,
+    lineHeight: 43.58,
+  },
+  h3: {
+    fontFamily: 'OpenSans-Regular',
+    fontSize: 24,
+    color: colors.black,
+    lineHeight: 32.68,
+  },
+};
